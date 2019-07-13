668dc5512d3e3cc3ee629ea2a6599ffa55cf20be
MagicMarvMan <marvin.kai@gmx.de>
2019-07-13 13:12:20 +0200

    Move javascript call to action

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ app.get("/admin", (req, res, next) => {
 </head>
 <body>
     <h1>${config.title}</h1>
-    <form action="javascript:void(0);" onsubmit="javascript:createCallback();">
+    <form action="javascript:createCallback();">
         <input type="text" placeholder="Zieladresse" id="targetInput"><br>
         <input type="submit" value="Erstellen">
     </form>
