a36ec409fc780aa855784f59aa0b309a8897fbf8
MagicMarvMan <marvin.kai@gmx.de>
2019-07-13 10:36:10 +0200

    Add link handler

--- src/index.js
+++ src/index.js
@@ -44,6 +44,47 @@ app.get("/", (req, res, next) => {
     }
 })
 
+app.get("/:shortcode", (req, res, next) => {
+    if(
+        (
+            models.Link.count({
+                where: {
+                    shortcode: req.params.shortcode
+                }
+            })
+        )
+        > 0
+    ) {
+        models.Link.findOne({
+            where: {
+                shortcode: req.params.shortcode
+            }
+        })
+            .then((link) => {
+                res.redirect(link.target)
+            })
+    } else {
+        res.status(404)
+        res.send(`
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>404 > nodeURL</title>
+    </head>
+    <body>
+        <h1>404 > nodeURL</h1>
+        <p>
+            <b>
+                The shortlink you requested could not be found.
+            </b>
+        </p>
+    </body>
+</html>
+        `)
+    }
+})
+
 
 // start app
 models.sequelize.sync().then(() => {
