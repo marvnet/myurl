a3b0896f56688e126a3749d15465fd975328606f
MagicMarvMan <marvin.kai@gmx.de>
2019-07-13 19:11:15 +0200

    Add navbar to routes

--- src/index.js
+++ src/index.js
@@ -37,6 +37,20 @@ app.get("/admin", (req, res, next) => {
     <link rel="stylesheet" type="text/css" href="${config.bootstrapbase}css/bootstrap.min.css">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/admin">${config.title}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/admin">Admin</a>
+            </li>
+        </ul>
+        </div>
+    </nav>
     <main>
         <div class="container">
             <h1>${config.title}</h1>
@@ -230,6 +244,20 @@ app.get("/:shortcode", (req, res, next) => {
         <link rel="stylesheet" type="text/css" href="${config.bootstrapbase}css/bootstrap.min.css">
     </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/admin">${config.title}</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/admin">Admin</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
         <main>
             <div class="container">
                 <p>
