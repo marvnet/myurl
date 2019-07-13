f4259007f10ed34ed93d45e3586dc5673ca613bb
MagicMarvMan <marvin.kai@gmx.de>
2019-07-13 13:24:44 +0200

    Fix domain config call

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ app.get("/admin", (req, res, next) => {
             xmlhttp.setRequestHeader("Content-Type", "application/json");
             xmlhttp.addEventListener("load", function (event) {
                 responseJson = JSON.parse(xmlhttp.responseText);
-                document.getElementById("response").innerHTML = 'Link created: <a href="http://' + config.domain + '/' + responseJson.shortcode + '">' + config.domain + '/' + responseJson.shortcode + '</a>';
+                document.getElementById("response").innerHTML = 'Link created: <a href="http://${config.domain}/' + responseJson.shortcode + '">${config.domain}/' + responseJson.shortcode + '</a>';
             });
             xmlhttp.send(JSON.stringify({target: target, key: "000000"}));
         }
